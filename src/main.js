a8b9574175af3db16f00ec1d9472b8ea0aa8a526
sohibjonabdialimov <sohibjonabdialimov6@gmail.com>
2024-02-19 21:43:38 +0500

    vue router

--- src/main.js
+++ src/main.js
@@ -1,5 +1,19 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import { createRouter, createWebHistory } from 'vue-router'
+import Home from "@/views/Home.vue"
+import About from "@/views/About.vue"
 
-createApp(App).mount('#app')
+const route = createRouter({
+  history: createWebHistory(),
+  routes: [
+    {
+      path: "/", name: "Home", component: Home
+    },
+    {
+      path: "/about", name: "About", component: About
+    }
+  ]
+})
+createApp(App).use(route).mount('#app')
--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-const path = require("path");
+import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
-  alias: {
-    '@' : path.resolve(__dirname, "./src")
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
   },
   plugins: [vue()],
 })
